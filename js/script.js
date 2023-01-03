76aaa6f1fd4aebcd9dd4c4ec185664ab1a81f2cf
Rajesh01-star <rajeshbehera489@gmail.com>
2023-01-03 17:43:42 +0530

    secret added with protection

--- js/script.js
+++ js/script.js
@@ -17,3 +17,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
     gal.appendChild(img);
   }
 });
+
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+let openBtn = document.getElementById("openBtn");
+let openBtnClass = document.querySelector(".openBtnClass");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openBtn.addEventListener("click", openModal);
+openBtnClass.addEventListener("click", openModal);
+document.addEventListener("keydown", function () {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+overlay.addEventListener("click", closeModal);
+
+document.getElementById("submit").addEventListener("click", function (event) {
+  event.preventDefault();
+  let paswd = document.getElementById("paswd");
+  if (paswd.value === "vibha") {
+    window.location.href = "/psswd.html";
+  }
+});
